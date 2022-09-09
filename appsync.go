50b423b0b23e104ed4d5b59b6610f6bedc995a27
Marcin Laube <marcin.laube@gmail.com>
2022-09-09 14:52:53 +0200

    release 1.0.0

--- appsync.go
+++ appsync.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
@@ -18,6 +17,14 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
+type HttpError struct {
+	Detail string `json:"detail"`
+}
+
+func (e *HttpError) Error() string {
+	return e.Detail
+}
+
 type AppSyncClient struct {
 	HttpClient  *http.Client
 	Credentials *aws.Credentials
@@ -26,42 +33,32 @@ type AppSyncClient struct {
 
 const SERVICE = "appsync"
 
-var GRAPHQL_API_ID = os.Getenv("API_HEATCTRL_GRAPHQLAPIIDOUTPUT")
 var GRAPHQL_ENDPOINT = os.Getenv("API_HEATCTRL_GRAPHQLAPIENDPOINTOUTPUT")
 var REGION = os.Getenv("REGION")
 
 func NewAppSyncClient() (*AppSyncClient, error) {
-
-	println("****")
-	println(GRAPHQL_API_ID)
-	println(GRAPHQL_ENDPOINT)
-	println(SERVICE)
-	println(REGION)
-	println("****")
-
 	credentials, err := RetrieveCredentials(context.Background())
 	if err != nil {
 		return nil, err
 	}
-	// https://christina04.hatenablog.com/entry/go-keep-alive
+
+	dia := (&net.Dialer{
+		Timeout:   10 * time.Second,
+		KeepAlive: 90 * time.Second,
+		DualStack: true,
+	}).DialContext
+	trans := &http.Transport{
+		DialContext:           dia,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          10,
+		MaxConnsPerHost:       100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
 	return &AppSyncClient{
-		&http.Client{
-			Transport: &http.Transport{
-				DialContext: (&net.Dialer{
-					Timeout:   10 * time.Second,
-					KeepAlive: 90 * time.Second,
-					DualStack: true,
-				}).DialContext,
-				ForceAttemptHTTP2:     true,
-				MaxIdleConns:          10,
-				MaxConnsPerHost:       100,
-				IdleConnTimeout:       90 * time.Second,
-				TLSHandshakeTimeout:   10 * time.Second,
-				ResponseHeaderTimeout: 10 * time.Second,
-				ExpectContinueTimeout: 1 * time.Second,
-			},
-			Timeout: 10 * time.Second,
-		},
+		&http.Client{Transport: trans, Timeout: 10 * time.Second},
 		credentials,
 		v4.NewSigner(),
 	}, nil
@@ -118,9 +115,10 @@ func (client *AppSyncClient) SendRequest(ctx context.Context, graphQL io.Reader)
 		return nil, err
 	}
 
-	if resp.StatusCode == http.StatusOK {
+	statusOK := resp.StatusCode >= 200 && resp.StatusCode < 300
+	if statusOK {
 		return respBody, nil
 	} else {
-		return nil, fmt.Errorf("Non-OK HTTP status: %v", resp.StatusCode)
+		return nil, &HttpError{http.StatusText(resp.StatusCode)}
 	}
 }
